b733f5cd3da0fdf13c6609a122876345fe5c4c34
mat28 <mathieu.roux21@gmail.com>
2016-11-13 22:01:29 +0100

    +first Commit

---
+++ api/routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+var ctrlHotels = require('../controllers/hotels.controllers.js');
+
+// Hotel routes
+router
+  .route('/hotels')
+  .get(ctrlHotels.hotelsGetAll)
+  .post(ctrlHotels.hotelsAddOne);
+/*
+router
+  .route('/hotels/:hotelId')
+  .get(ctrlHotels.hotelsGetOne)
+  .put(ctrlHotels.hotelsUpdateOne)
+  .delete(ctrlHotels.hotelsDeleteOne);
+*/
+module.exports = router;
